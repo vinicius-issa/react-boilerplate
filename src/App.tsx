9818be8b25321fab576d41093d7ba8ea1d517e41
Vinicius Issa <vinicius.issa@ab-inbev.com>
2023-07-08 10:55:26 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useMemo, useState } from "react"
+import "./App.scss"
+
+export default function App() {
+  return <>Hello World</>
+}
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const dotenv = require("dotenv");
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { DefinePlugin } = require("webpack");
+
+const fileEnv = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before (but with the variables from the file)
+const envKeys = fileEnv
+  ? Object.keys(fileEnv).reduce((prev, next) => {
+      prev[`${next}`] = JSON.stringify(fileEnv[next]);
+
+      return prev;
+    }, {})
+  : {};
+
+const config = {
+  "process.env": envKeys,
+};
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+    onListening(server) {
+      console.log("Listening on port:", 3000);
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".css"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      filename: "index.html", // output file
+      template: path.join("public", "index.html"),
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new DefinePlugin(config),
+  ],
+  devtool: "inline-source-map",
+};
